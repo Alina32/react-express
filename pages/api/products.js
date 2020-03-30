3419da5dc53ce8c2753fa6f447b81188a8050ba6
Alina32 <actinoid34@gmail.com>
2020-03-30 15:45:36 +0300

    react-reserv

---
+++ pages/api/products.js
@@ -0,0 +1,11 @@
+//import products from '../../static/products.json';
+import Product from '../../models/Product'; 
+import connectDB from '../../utils/connectDB';
+
+
+connectDB();
+
+export default async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json(products);
+};
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
+
+function Home({ products }) {
+  return <ProductList products={products}/>
+};
+
+Home.getInitialProps = async () => {
+
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data }; 
+};
+
+export default Home; 
