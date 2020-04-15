db3c4ac62056607470a8947cac2dff2aea0381e5
Alina32 <actinoid34@gmail.com>
2020-04-15 12:22:06 +0300

    hotelsslider

--- components/Rooms/HotelSlider.js
+++ components/Rooms/HotelSlider.js
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import { Item, Rating, Header, Container, Segment } from 'semantic-ui-react';
 
 
-export default class HotelSlid extends Component {
+export default class HotelSlider extends Component {
   constructor(props) {
     super(props);
     this.state = {
